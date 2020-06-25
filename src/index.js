e2dd649eb61769649304e68d18c8139b1fbfa39e
nsalvador <nigelsalvador@gmail.com>
2020-06-25 15:47:25 -0400

    Inital commit & setup routing

--- src/index.js
+++ src/index.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import './index.css';
-import App from './App';
+
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const DashboardPage = () => <div>This is from my dashboard component</div>;
+
+const SearchPage = () => <div>This is from my search component</div>;
+
+const routes = (
+	<BrowserRouter>
+		<>
+			<Route path="/" component={DashboardPage} exact={true} />
+			<Route path="/search" component={SearchPage} />
+		</>
+	</BrowserRouter>
 );
+
+ReactDOM.render(routes, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
